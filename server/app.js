a70732716323a4344c021c137acc20c6b9af7d89
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-23 01:56:18 -0700

    alternate way

--- server/app.js
+++ server/app.js
@@ -5,8 +5,11 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
-app.get('/throw-error', (req, res) => {
-  throw new Error('An error occurred!');
+// https://github.com/nathanieldcooke/week10-day2/blob/main/practice-for-week-10-express-error-handling-404/server/app.js
+// Custom error handler.
+app.use((req, res, next) => {
+  const err = { message: 'blah' };
+  next(err);
 });
 
 // https://stackoverflow.com/questions/11500204/how-can-i-get-express-js-to-404-only-on-missing-routes
@@ -18,7 +21,8 @@ app.get('/throw-error', (req, res) => {
 //     next(err);
 //   }
 // });
-app.use((req, res, next) => {
+app.use((err, req, res, next) => {
+  console.error(err);
   res.status(404).json({
     message: "Sorry, the requested resource couldn't be found",
     statusCode: 404
